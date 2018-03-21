5878883945f042e74191f0096b9d9d3bb6aa625a
Tim Morgan <tim@squareup.com>
2018-03-20 21:17:05 -0700

    Explicitly allow front-end routes for HTML and */*; all other routes 404

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  constraints(->(req) { req.format == :json || req.format == :rss }) do
-    resources :episodes, only: %i[index show create update destroy]
-    resource :session, only: %i[show create destroy]
-  end
-
-  root 'home#index'
-
   get '/rails/active_storage/transcoded/:signed_blob_id/:encoding_key/*filename' =>
           'transcoding#show',
-      as:       :rails_blob_transcoding,
-      internal: true
+      as:                                                                        :rails_blob_transcoding,
+      internal:                                                                  true
 
   direct :rails_transcoding do |transcoder, options|
     signed_blob_id = transcoder.blob.signed_id
@@ -29,6 +22,19 @@ Rails.application.routes.draw do
     mount Sidekiq::Web => '/sidekiq'
   end
 
-  match '*path' => 'home#index', via: :all, constraints: ->(req) { req.format == :html }
-  # Vue-Router handles all the front-end routing (routes.js)
+  # back-end routes handled by rails
+  constraints(->(req) { req.format == :json || req.format == :rss }) do
+    resources :episodes, only: %i[index show create update destroy]
+    resource :session, only: %i[show create destroy]
+  end
+
+  # front-end routes handled by Vue-Router (routes.js)
+  constraints(->(req) { req.format == '*/*' || req.format.html? }) do #TODO .all? doesn't work here
+    root 'home#index'
+    get 'upload' => 'home#index'
+    get 'episodes/:id' => 'home#index'
+    get 'episodes/:id/edit' => 'home#index'
+    get 'episodes/:id/script' => 'home#index'
+    get 'about' => 'home#index'
+  end
 end
