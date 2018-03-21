becceecdea32cf1e70b04f76aacaa5829da66de9
Tim Morgan <tim@squareup.com>
2018-03-20 20:36:00 -0700

    Spec fix

--- spec/controllers/transcoding_controller_spec.rb
+++ spec/controllers/transcoding_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe TranscodingController, type: :controller do
       signed_path = polymorphic_url(@episode.audio.transcode('mp3', %w[-ac 1]), host: 'test.host')
       parts = signed_path.split('/')
       get :show, params: {signed_blob_id: parts[6], encoding_key: parts[7], filename: parts[8]}
-      expect(response).to redirect_to(/http:\/\/test\.host\/rails\/active_storage\/disk\/.+\/audio\.mp3\?content_type=audio%2Fmp3&disposition=inline%3B\+filename%3D%22audio\.mp3%22%3B\+filename%2A%3DUTF-8%27%27audio\.mp3$/)
+      expect(response.headers['Location']).to match(/http:\/\/test\.host\/rails\/active_storage\/disk\/.+\/audio\.mp3\?content_type=audio%2Fmpeg&disposition=inline%3B\+filename%3D%22audio\.mp3%22%3B\+filename%2A%3DUTF-8%27%27audio\.mp3$/)
     end
 
     it "should render a 404 for an unknown blob" do
